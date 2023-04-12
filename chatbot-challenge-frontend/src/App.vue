a3cbf438c0f546b38b0cba8cfc95de69e8ce9cd8
Jan Nehring <jan.nehring@dfki.de>
2023-04-12 08:29:48 +0200

    update design

--- chatbot-challenge-frontend/src/App.vue
+++ chatbot-challenge-frontend/src/App.vue
@@ -4,25 +4,9 @@ import ListRooms from './components/ListRooms.vue'
 </script>
 
 <template>
-  <header>
-      <div class="wrapper">
-          <div class="header">
-          <h1>Chatbot Challenge</h1>
-          <ul class="nav">
-            <li>
-              <RouterLink to="/">Escape Rooms</RouterLink>
-              </li>
-            <li>
-              <RouterLink to="/about">About</RouterLink>
-              </li>
-          </ul>
-          <p>&nbsp;</p>
-          <p>&nbsp;</p>
-        </div>
-        <RouterView />
-
-      </div>
-  </header>
+    <div class="wrapper">
+      <RouterView />
+    </div>
 
 </template>
 
--- chatbot-challenge-frontend/src/components/ListRooms.vue
+++ chatbot-challenge-frontend/src/components/ListRooms.vue
@@ -3,12 +3,24 @@ import { config } from '../../config'
 </script>
 
 <template>
-  <h2>Choose your escape room</h2>
-  <p>&nbsp;</p>
-  <div class="rooms">
-    <template v-for="room in config.rooms">
-      <a :href="'/room/' + room.id">{{ room.name }}</a>
-    </template>
-  </div>
+    <div class="content-box">
+        <h1>Chatbot Challenge</h1>
+        <!--<ul class="nav">
+          <li>
+            <RouterLink to="/">Escape Rooms</RouterLink>
+            </li>
+          <li>
+            <RouterLink to="/about">About</RouterLink>
+            </li>
+        </ul>-->
+        <p>&nbsp;</p>
+
+        <h2>Choose your escape room</h2>
+        <div class="rooms">
+          <template v-for="room in config.rooms">
+            <a :href="'/room/' + room.id">{{ room.name }}</a>
+          </template>
+        </div>
+    </div>
 </template>
 
