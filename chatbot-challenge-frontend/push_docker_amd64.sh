1b6d4459ab8327c34e563edd8bed57fab29e26db
jan <asd@asd.de>
2024-04-09 16:38:47 +0200

    fix push frontend script

--- chatbot-challenge-frontend/push_docker_amd64.sh
+++ chatbot-challenge-frontend/push_docker_amd64.sh
@@ -1,8 +1,8 @@
 cd "$(dirname "$0")"
 
-docker build -t chatbot-challenge-frontend-amd64 --platform linux/amd64/v3 .
-img="vm014.qu.tu-berlin.de:6000/chatbot-challenge-frontend-amd64"
-docker tag chatbot-challenge-frontend-amd64 $img
+docker build -t csd-frontend-amd64 --platform linux/amd64/v3 .
+img="localhost:6000/csd-frontend-amd64"
+docker tag csd-frontend-amd64 $img
 docker push $img
 
 
--- chatbot-challenge-frontend/push_docker_arm64.sh
+++ chatbot-challenge-frontend/push_docker_arm64.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 
-docker build -t chatbot-challenge-frontend-arm64 .
-img="vm014.qu.tu-berlin.de:6000/chatbot-challenge-frontend-arm64"
-docker tag chatbot-challenge-frontend-arm64 $img
+docker build -t csd-frontend-arm64 .
+img="vm014.qu.tu-berlin.de:6000/csd-frontend-arm64"
+docker tag csd-frontend-arm64 $img
 docker push $img
 
