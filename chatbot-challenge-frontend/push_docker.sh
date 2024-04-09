a3701408ef96bf71a1a966ecbce94618866f0bd3
Jan Nehring <jan.nehring@dfki.de>
2024-04-09 11:34:45 +0200

    add docker script

---
+++ chatbot-challenge-frontend/push_docker.sh
@@ -0,0 +1,13 @@
+cd "$(dirname "$0")"
+
+docker build -t chatbot-challenge-frontend-amd64 --platform linux/amd64/v3 .
+img="vm014.qu.tu-berlin.de:6000/chatbot-challenge-frontend-amd64"
+docker tag chatbot-challenge-frontend-amd64 $img
+docker push $img
+
+
+docker build -t chatbot-challenge-frontend-arm64 .
+img="vm014.qu.tu-berlin.de:6000/chatbot-challenge-frontend-arm64"
+docker tag chatbot-challenge-frontend-arm64 $img
+docker push $img
+
